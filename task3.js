64f72d51cf38c5cd9bb29ee01a08ec98de9a3f5b
pahlavonjon <pahlavonjon.odilov@student.adelaide.edu.au>
2023-03-29 23:09:28 +1030

    added

--- task3.js
+++ task3.js
@@ -11,40 +11,7 @@ function get_user_input(){
 function get_the_time () {
     document.getElementsByClassName("post-time")[0].innerText = Date();
 }
-function post_multiple(){
-    var the_quantity_value = document.getElementById("quantity");
-    for (var k = 1; k <= the_quantity_value.value; k++ ){
-        var posts_div = document.createElement('div');
-        var time_p = document.createElement('p');
-        var text_p = document.createElement('p');
-        document.body.append(posts_div);
-        document.body.append(time_p);
-        document.body.append(text_p);
-        var farben = document.getElementsByClassName("farben");
-        var typ = document.getElementsByClassName("typ");
-        posts_div.classList.add("new_post_div");
-        time_p.classList.add("post-time");
-        text_p.classList.add("post-content");
-        time_p.innerText = Date();
-        text_p.innerText = document.getElementsByTagName("textarea")[0].value;
-        if (farben[0].checked == true){
-            text_p.style.color = "blue";
-        }
-        if (farben[1].checked == true){
-            text_p.style.color = "red";
-        }
-        if (typ[0].checked == true){
-            text_p.style.fontWeight = "bold";
-        }
-        if (typ[1].checked == true){
-            text_p.style.fontWeight = "italic";
-        }
-        posts_div.append(time_p);
-        posts_div.append(text_p);
-        var the_post_destination = document.getElementById("posts");
-        the_post_destination.appendChild(posts_div);
-    }
-}
+
 var the_back = document.getElementById("menu");
 the_back.style.display = 'none';
 function remove_front() {
@@ -73,5 +40,40 @@ function change_the_text_color_red(){
 function background_color_change(){
     var the_background = document.getElementById("background_input").value;
     document.body.style.backgroundColor = the_background;
+}
+
+function post_multiple(){
+    let the_quantity_value = document.getElementById("quantitys");
+    for (var k = 1; k <= the_quantity_value.value; k++ ){
+        const posts_div = document.createElement('div');
+        const time_p = document.createElement('p');
+        const text_p = document.createElement('p');
+        document.body.append(posts_div);
+        document.body.append(time_p);
+        document.body.append(text_p);
+        const farben = document.getElementsByClassName("farben");
+        const typ = document.getElementsByClassName("typ");
+        time_p.classList.add("post-time");
+        text_p.classList.add("post-content");
+        posts_div.classList.add("new_post_div");
+        time_p.innerText = Date();
+        text_p.innerText = document.getElementsByTagName("textarea")[0].value;
+        if (farben[0].checked == true){
+            text_p.style.color = "blue";
+        }
+        if (farben[1].checked == true){
+            text_p.style.color = "red";
+        }
+        if (typ[0].checked == true){
+            text_p.style.fontWeight = "bold";
+        }
+        if (typ[1].checked == true){
+            text_p.style.fontWeight = "italic";
+        }
+        posts_div.append(time_p);
+        posts_div.append(text_p);
+        var the_post_destination = document.getElementById("posts");
+        the_post_destination.appendChild(posts_div);
+    }
 }
 
