37142291eb60b3d5b83af6ffc7cfeb7fdc327fec
pahlavonjon <pahlavonjon.odilov@student.adelaide.edu.au>
2023-03-29 17:16:25 +1030

    added

--- task3.js
+++ task3.js
@@ -6,42 +6,44 @@ function mcount() {
 }
 
 
+
 // Task  3.2
 
 function get_user_input(){
     var result = document.querySelector("#post");
     result = document.querySelector(".post-content");
-    result.innerText = document.getElementById("text_area").innerText;
-
-//    // var result = document.querySelectorAll("#post post-content");
-//     console.log(result);
-//     result.innerHTML = document.getElementsByClassName("textarea").innerHTML;
-
-//     console.log();
+    result.innerText = document.getElementById("text_area").value;
 
 }
-
-function gleichzeitig_input() {
-    var result = document.querySelector("#post");
-    result = document.querySelector(".post-content");
-    var the_letter = result.innerText;
-    result.innerText = the_letter;
-  }
 
 function get_the_time () {
     document.getElementsByClassName("post-time")[0].innerText = Date();
 }
 
+
+
 //  Task 3.3 
 
-document.getElementById("menu").style.display = "none";
+var the_back = document.getElementById("menu");
+the_back.style.display = 'none';
 
 function remove_front() {
     var rest_of_the_page = document.getElementById("main");
     rest_of_the_page.style.display = "none";
     var the_back_menu = document.getElementById("menu");
-    the_back_menu.style.visibility = "visible";
+    the_back_menu.style.display = 'inline';
+
 }
+
+function show_front(){
+    var the_back_menu = document.getElementById("menu");
+    the_back_menu.style.display = 'none';
+    var rest_of_the_page = document.getElementById("main");
+    rest_of_the_page.style.display = 'inline';
+}
+
+
+
 
 //  3.4
 
