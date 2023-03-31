f07d00df88390ab747ebf2e5b76d9836c0badd4b
pahlavonjon <pahlavonjon.odilov@student.adelaide.edu.au>
2023-03-31 21:26:53 +1030

    added

--- task1.js
+++
@@ -1,9 +0,0 @@
-
-
-
-document.getElementById("the_button").addEventListener("click",change_the_time);
-
-function change_the_time() {
-    document.getElementById("current_time").innerHTML = Date();
-
-}
