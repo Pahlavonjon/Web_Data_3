8fe795c141e9561f28af89bac2335f9cbf94c6c0
pahlavonjon <pahlavonjon.odilov@student.adelaide.edu.au>
2023-03-31 22:44:32 +1030

    added

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require('express');
+const app = express();
+const path = require('path')
 var router = express.Router();
 
 
+router.get('/',function(req,res){
+    sendFile('index.html');
+});
 
+// console.log('server sending html file');
 
 
 
