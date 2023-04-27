9dcaee6fe7353d10414e68beb319efbe474ad876
Pahlavonjon Odilov <Pahlavonjon777@gmail.com>
2023-04-27 19:18:58 +0930

    added

--- query3-5.sql
+++ query3-5.sql
@@ -1,7 +1,4 @@
 -- Active: 1682041160807@@127.0.0.1@3306@enrolment
-select given_name, family_name from Students
-where student_id in (SELECT student_id FROM Enrolments
-WHERE Enrolments.mark < 50 and Enrolments.subject_code = "COMP SCI 2000");
 
 
 
